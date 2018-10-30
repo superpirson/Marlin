6c6a32151aef439573e6f6b6e27674b8ad4e0862
Alex Patton <superpirson@gmail.com>
2018-10-30 17:25:16 -0400

    small updates

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -775,7 +775,7 @@
 */
 #define X_PROBE_OFFSET_FROM_EXTRUDER 6  // X offset: -left  +right  [of the nozzle]
 #define Y_PROBE_OFFSET_FROM_EXTRUDER -48  // Y offset: -front +behind [the nozzle]
-#define Z_PROBE_OFFSET_FROM_EXTRUDER -2   // Z offset: -below +above  [the nozzle]
+#define Z_PROBE_OFFSET_FROM_EXTRUDER -1   // Z offset: -below +above  [the nozzle]
 
 // Certain types of probes need to stay away from edges
 #define MIN_PROBE_EDGE 5
