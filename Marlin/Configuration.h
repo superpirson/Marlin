ce10eb449fad780d3ebb85984cd945f8cb3508a1
Alex Patton <superpirson@gmail.com>
2018-11-22 11:36:59 -0600

    tweaked configs

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -362,14 +362,14 @@
 // Comment the following line to disable PID and enable bang-bang.
 #define PIDTEMP
 
-#define BANG_MAX 255     // Limits current to nozzle while in bang-bang mode; 255=full current
-#define PID_MAX 255 // Limits current to nozzle while PID is active (see PID_FUNCTIONAL_RANGE below); 255=full current
+#define BANG_MAX 160    // Limits current to nozzle while in bang-bang mode; 255=full current
+#define PID_MAX 170 // Limits current to nozzle while PID is active (see PID_FUNCTIONAL_RANGE below); 255=full current
 #define PID_K1 0.95      // Smoothing factor within any PID loop
 #if ENABLED(PIDTEMP)
   #define PID_AUTOTUNE_MENU // Add PID Autotune to the LCD "Temperature" menu to run M303 and apply the result.
   //#define PID_DEBUG // Sends debug data to the serial port.
   //#define PID_OPENLOOP 1 // Puts PID in open loop. M104/M140 sets the output power from 0 to PID_MAX
-  //#define SLOW_PWM_HEATERS // PWM with very low frequency (roughly 0.125Hz=8s) and minimum state time of approximately 1s useful for heaters driven by a relay
+  //#define SLOW_PWM_HEATERS // PWM with ver255y low frequency (roughly 0.125Hz=8s) and minimum state time of approximately 1s useful for heaters driven by a relay
   //#define PID_PARAMS_PER_HOTEND // Uses separate PID parameters for each extruder (useful for mismatched extruders)
                                   // Set/get with gcode: M301 E[extruder number, 0-2]
   #define PID_FUNCTIONAL_RANGE 30 // If the temperature difference between the target temperature and the actual temperature
@@ -378,9 +378,9 @@
   // If you are using a pre-configured hotend then you can use one of the value sets by uncommenting it
 
   // Ultimaker
-  #define DEFAULT_Kp 10.86
-  #define DEFAULT_Ki 0.72
-  #define DEFAULT_Kd 40.94
+  #define DEFAULT_Kp 30.20
+  #define DEFAULT_Ki 2.84
+  #define DEFAULT_Kd 80.39
 
   // MakerGear
   //#define DEFAULT_Kp 7.0
