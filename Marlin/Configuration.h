28c0210138ab8e6c3b35aab9748e3fb82db390f9
Alex Patton <superpirson@gmail.com>
2018-11-22 11:45:04 -0600

    reset the z home position to 0. Good god why did I change it?

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -882,7 +882,7 @@
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS -15
 #define Y_MIN_POS 0
-#define Z_MIN_POS -1
+#define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE //+ 25 //NOTE: This is critical to mantain the spacing of the bed, as the endstop is not the edge of bed
 
