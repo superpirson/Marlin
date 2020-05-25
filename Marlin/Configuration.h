60d6e1010e23360230b5dd5e84fa0b5ef1d972d8
Alex <superpirson@gmail.com>
2020-05-25 14:32:32 -0700

    fix steps per MM setting beign wrong due to using emperical measurements

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -605,7 +605,7 @@
    Override with M92
                                         X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
 */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   {416.0887, 416.0887, 832.1774, 451.2855 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   {400.0, 400.0, 800.0, 451.2855 }
 
 /**
    Default Max Feed Rate (mm/s)
